247fefd3425ee1eb6d6e5458ed52181b83dbdbf2
Roman Marchenko <rmarchenko@azul.com>
2023-04-06 08:20:50 +0000

    Fixing review comments

--- criu/pages-compress.c
+++ criu/pages-compress.c
@@ -39,11 +39,11 @@ int compress_images(void)
 
 	pr_debug("Compressing pages\n");
 
-	sprintf(format, "%s/%s", "/proc/%ld/fd/%d", imgset_template[CR_FD_PAGES].fmt);
-	sprintf(pathsrc, format, pid, dfd, id);
+	snprintf(format, sizeof(format), "%s/%s", "/proc/%ld/fd/%d", imgset_template[CR_FD_PAGES].fmt);
+	snprintf(pathsrc, sizeof(pathsrc), format, pid, dfd, id);
 
-	sprintf(format, "%s/%s", "/proc/%ld/fd/%d", imgset_template[CR_FD_PAGES_COMP].fmt);
-	sprintf(pathdst, format, pid, dfd, id);
+	snprintf(format, sizeof(format), "%s/%s", "/proc/%ld/fd/%d", imgset_template[CR_FD_PAGES_COMP].fmt);
+	snprintf(pathdst, sizeof(pathdst), format, pid, dfd, id);
 
 	ret = LZ4IO_compressFilename(pathsrc, pathdst, 8, lz4_prefs);
 	LZ4IO_freePreferences(lz4_prefs);
@@ -67,7 +67,7 @@ static int decompress_image(int comp_fd) {
 	char outbuf[outbufsize];
 	LZ4F_errorCode_t lz4err;
 	LZ4F_decompressionContext_t dctx;
-	int ret;
+	int decomp_fd;
 	size_t totalread = 0;
 	size_t totalwrite = 0;
 	int bytestoread = compbufsize;
@@ -80,21 +80,28 @@ static int decompress_image(int comp_fd) {
 	}
 
 	pr_debug("Creating file at '%s'\n", temp_path_decompressed);
-	ret = open(temp_path_decompressed, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	decomp_fd = open(temp_path_decompressed, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (0 > ret) {
+	if (0 > decomp_fd) {
 		pr_err("Can't create file, errno=%d\n", errno);
 		LZ4F_freeDecompressionContext(dctx);
 		return -1;
 	}
 
 	while (1) {
-		const size_t readbytes = read(comp_fd, compbuf + offset, bytestoread);
-		totalread += readbytes;
+		const ssize_t readbytes = read(comp_fd, compbuf + offset, bytestoread);
+		if (0 > readbytes) {
+			if (EINTR == errno) {
+				continue;
+			}
+			pr_perror("failed to read compressed data");
+			break; // stop on error
+		}
 		if (!readbytes && 0 == offset) {
 			/* reached end of file or stream */
 			break;
 		}
+		totalread += readbytes;
 		{
 			size_t outsize = sizeof outbuf;
 			size_t insize = offset + readbytes;
@@ -105,19 +112,41 @@ static int decompress_image(int comp_fd) {
 				break;
 			}
 			{
-				const ssize_t res = write(ret, outbuf, outsize);
-				if (0 > res) {
-					pr_err("write error to output file, fd=%d, errno=%d\n", ret, errno);
-					break;
-				}
+				// Write decompressed bytes
+				ssize_t bytes_written = 0;
+				ssize_t out_offset = 0;
+				do {
+					bytes_written = write(decomp_fd, outbuf + out_offset, outsize);
+					if (0 > bytes_written) {
+						if (EINTR == errno) {
+							continue;
+						}
+						pr_perror ("write error to decompressed file");
+						break;
+					}
+					out_offset += bytes_written;
+					if (outsize < (size_t)bytes_written) {
+						pr_err("written more bytes in total than needed, extected=%lu, written=%lu\n",
+						       (unsigned long)outsize, (unsigned long)bytes_written);
+						break;
+					}
+					outsize -= bytes_written;
+					totalwrite += bytes_written;
+				} while (0 < outsize);
 			}
-			totalwrite += outsize;
+			if (0 < outsize) {
+				pr_err("writing is not completed, outsize=%lu\n", (unsigned long)outsize);
+				break;
+			}
 			offset = insize_orig - insize;
 			bytestoread = insize;
 			memmove(compbuf, compbuf + compbufsize - offset, offset);
 		}
 	}
-	close(ret);
+	if (0 > close(decomp_fd)) {
+		pr_perror("failed closing decompressed file");
+	}
+	LZ4F_freeDecompressionContext(dctx);
 	pr_debug("decompression completed, read %lu, wrote %lu\n", (unsigned long)totalread, (unsigned long)totalwrite);
 	return 0;
 }
@@ -148,7 +177,7 @@ int decompression_thread_start(void) {
 	}
 
 	pr_debug("Opening compressed image...\n");
-	sprintf(image_path, imgset_template[CR_FD_PAGES_COMP].fmt, 1);
+	snprintf(image_path, sizeof(image_path), imgset_template[CR_FD_PAGES_COMP].fmt, 1);
 	comp_fd = openat(dfd, image_path, O_RDONLY, CR_FD_PERM);
 	if (0 > comp_fd) {
 		pr_warn("Can't open '%s' for dfd=%d, errno=%d\n", image_path, dfd, errno);
@@ -200,7 +229,7 @@ int decompression_thread_join(void) {
 int decompression_get_fd(void) {
 	int ret;
 	pr_debug("wait for semaphore...\n");
-	ret = open(temp_path_decompressed, O_RDONLY, 0600);
+	ret = open(temp_path_decompressed, O_RDONLY, S_IRUSR | S_IWUSR);
 	if (0 > ret) {
 		pr_err("Failed open %s errno=%d\n", temp_path_decompressed, errno);
 	}
